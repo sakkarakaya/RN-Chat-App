e550cca619ed3b64c9a38205c39304d1470aa837
sakkarakaya <sakkarakaya@icloud.com>
2021-07-01 22:34:25 +0200

    added Router

---
+++ src/Router.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Login, Timeline, Sign } from "./pages";
+
+const Stack = createStackNavigator()
+
+
+const Router = () => {
+
+    return (
+
+        <NavigationContainer>
+            
+        </NavigationContainer>
+    )
+}
+
+export default Router
